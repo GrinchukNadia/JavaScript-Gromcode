1b599dee55dfdcd89b5ba7b57aa2f03f588094aa
Надежда <grennad991@gmail.com>
2021-09-20 18:54:16 +0300

    changed console.log for console.dir

--- 21_lesson/02_task/index.js
+++ 21_lesson/02_task/index.js
@@ -1,12 +1,12 @@
 export function getTitleElement() {
   const titleElem = document.querySelector('title')
-  console.log(titleElem)
+  console.dir(titleElem)
   return titleElem
 }
 
 export function getInputElement() {
   const inputElem = document.querySelector('input[type="text"]')
-  console.log(inputElem)
+  console.dir(inputElem)
   return inputElem
 }
 
