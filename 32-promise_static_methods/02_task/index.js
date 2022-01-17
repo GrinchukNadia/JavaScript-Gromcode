0327228ece047218373ddc1175697de3b50aad60
Надежда <grennad991@gmail.com>
2022-01-17 10:31:36 +0200

    fixed misspell in variable

--- 32-promise_static_methods/02_task/index.js
+++ 32-promise_static_methods/02_task/index.js
@@ -1,29 +1,28 @@
+const getRandomNumber = (from, to) => from + Math.random() * (to - from);
+const request = (url) =>
+  new Promise((resolve) => {
+    const randomDelay = getRandomNumber(1000, 3000);
+    setTimeout(() => {
+      resolve({
+        userData: {
+          name: 'Tom',
+          age: 17,
+        },
+        source: url,
+      });
+    }, randomDelay);
+  });
+
 const servers = [
   'https://server.com/us',
   'https://server.com/eu',
   'https://server.com/au',
 ];
-
-const randomDelay = (from, to) => Math.ceil(from + Math.random() * (to - from));
-
-const request = url => new Promise(resolve => {
-  const delay = randomDelay(1000, 3000)
-  setTimeout(() => {
-    resolve({
-      userData: {
-        name: 'Tom',
-        age: 17,
-      },
-      sourse: url
-    })
-  }, delay)
-})
-
 export const getUserASAP = (userId) => {
-  const userRequests = servers
-    .map(server => request(`${server}/${userId}`))
+  const userUrls = servers.map((serverUrl) => `${serverUrl}/users/${userId}`);
 
-    return Promise.race(userRequests);
-}
+  const requests = userUrls.map((userUrl) => request(userUrl));
 
-getUserASAP('user_1').then(result => console.log(result))
+  return Promise.race(requests);
+};
+getUserASAP('user-id-1').then((res) => console.log(res));
