468f4a8781fd50b8f586a0edad87af77ee180e8a
Надежда <grennad991@gmail.com>
2022-01-22 17:59:12 +0200

    refactoring

--- 34-complex_practice_with_HTTP/02_task/index.js
+++ 34-complex_practice_with_HTTP/02_task/index.js
@@ -6,36 +6,34 @@ const submitBtnElem = document.querySelector('.submit-button');
 const baseUrl =
   'https://61eaff907ec58900177cdb49.mockapi.io/api/v1/registration';
 
-// let userData = {};
-
-formElem.addEventListener('input', (event) => {
-  event.preventDefault();
+const enableBtnOnValid = () =>{
   submitBtnElem.disabled = !formElem.reportValidity();
-});
+}
 
-formElem.addEventListener('submit', (event) => {
+const onSubmit = (event) => {
   event.preventDefault();
   const userData = {
     name: nameElem.value,
     email: emailElem.value,
     password: passwordElem.value,
   };
-  
+
   fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
     },
     body: JSON.stringify(userData),
-  }).then((response) => response.json())
-  .then(data => alert(JSON.stringify(data)))
-  .then(() => {
-    nameElem.value = '';
-    emailElem.value = '';
-    passwordElem.value = '';
-    submitBtnElem.disabled = !formElem.reportValidity();
-  });
-});
+  })
+    .then((response) => response.json())
+    .then((data) => alert(JSON.stringify(data)))
+    .then(() => {
+      nameElem.value = '';
+      emailElem.value = '';
+      passwordElem.value = '';
+      enableBtnOnValid();
+    });
+};
 
-submitBtnElem.addEventListener('click', () => {
-})
+formElem.addEventListener('input', enableBtnOnValid);
+formElem.addEventListener('submit', onSubmit);
