02174e3cc85efcbdcdef06869564848663ec2f83
Надежда <grennad991@gmail.com>
2022-01-24 19:36:28 +0200

    refactoring

--- 35-error_handling/03_task/src/index.js
+++ 35-error_handling/03_task/src/index.js
@@ -2,20 +2,23 @@ import { fetchUserData, fetchUserRepo } from "./fetchUserData.js";
 import { renderDefaultPhoto, renderUserData } from "./renderUserData.js";
 import { showSpinner } from "./spinner.js";
 
-renderDefaultPhoto();
 
-const onSearchUser = () => {
-  const userNameInputElem = document.querySelector('.name-form__input');
-  const userName = userNameInputElem.value;
-  fetchUserData(userName)
-    .then((userData) => {
-      showSpinner();
-      fetchUserRepo(userData);
-      renderUserData(userData);
-    })
-    .catch((e) => alert('Failed to load data'));
-  // .finally();
-};
-
-const showUserBtnElem = document.querySelector('.name-form__btn');
-showUserBtnElem.addEventListener('click', onSearchUser);
+window.addEventListener('DOMContentLoaded', () => {
+  renderDefaultPhoto();
+  
+  const onSearchUser = () => {
+    const userNameInputElem = document.querySelector('.name-form__input');
+    const userName = userNameInputElem.value;
+    fetchUserData(userName)
+      .then((userData) => {
+        showSpinner();
+        fetchUserRepo(userData);
+        renderUserData(userData);
+      })
+      .catch((e) => alert('Failed to load data'));
+    // .finally();
+  };
+  
+  const showUserBtnElem = document.querySelector('.name-form__btn');
+  showUserBtnElem.addEventListener('click', onSearchUser);
+})
